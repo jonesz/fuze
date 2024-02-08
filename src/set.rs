c2fe333b4bfb5b39588e61ff750242147d895b8e
Ethan Jones <etn.jones@gmail.com>
2024-02-08 15:49:14 -0600

    Compute the intersection of two intervals.
    Commit introduces the basics needed for a DST implementation.

--- src/set.rs
+++ src/set.rs
@@ -31,12 +31,21 @@ mod interval {
 
     impl<T: Ord + Copy, const N: usize> Interval<T, N> {
         fn intersect(lhs: &(T, T), rhs: &(T, T)) -> Option<(T, T)> {
-            // Check to see if there's any overlap between the intervals.
-            if !(lhs.1 > rhs.0 || rhs.1 > lhs.1) {
+            // Place the interval with the smallest left bound on the left.
+            let (lhs, rhs) = if lhs.0 <= rhs.0 {
+                (lhs, rhs)
+            } else {
+                (rhs, lhs)
+            };
+
+            // If there's no overlap between the intervals, lhs.1 should be < rhs.0.
+            if lhs.1 < rhs.0 {
                 return None;
             }
 
-            todo!("Left unimplemented.");
+            // Otherwise, the intersection is the maximum of the left bound and the minimum
+            // of the right.
+            Some((lhs.0.max(rhs.0), lhs.1.min(rhs.1)))
         }
     }
 
@@ -66,7 +75,7 @@ mod interval {
             for (idx, mem) in buf.iter_mut().enumerate() {
                 let (left, right) = (self.buf.get(idx).unwrap(), rhs.buf.get(idx).unwrap());
                 match (left, right) {
-                    (Some(l), Some(r)) => todo!(),
+                    (Some(l), Some(r)) => *mem = Self::intersect(&l, &r),
                     _ => *mem = None,
                 }
             }
@@ -83,15 +92,19 @@ mod interval {
             // Intervals are disjoint.
             let (a, b) = ((-1i32, 0i32), (1i32, 2i32));
             assert!(Interval::<i32, 1>::intersect(&a, &b).is_none());
+
             // Interval a is contained within interval b.
             let (a, b) = ((-1i32, 1i32), (-2i32, 2i32));
             assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == a));
+
             // Interval b is contained within interval a.
             let (a, b) = ((-2i32, 2i32), (-1i32, 1i32));
             assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == b));
+
             // Interval a overlaps with interval b on the left side.
             let (a, b) = ((-2i32, 1i32), (0i32, 2i32));
             assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == (0i32, 1i32)));
+
             // Interval b overlaps with interval a on the left side.
             let (a, b) = ((-2i32, 1i32), (-3i32, 0i32));
             assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == (-2i32, 0i32)));
