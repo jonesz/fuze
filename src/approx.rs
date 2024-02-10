a64d5ae1c1e533dd08f83e096a351c48455715ee
Ethan Jones <etn.jones@gmail.com>
2024-02-10 11:11:48 -0600

    Rename `Magnitude` to BPQ; build a working implementation.
    As noted, there's a potential issue when we call `into_iter` when the
    BPQ isn't entirely initialized. A solution might to utilize the slice
    from `[0..num_initialized]`, but we still run into an issue where the
    values aren't sorted.

--- src/approx.rs
+++ src/approx.rs
@@ -1,7 +1,7 @@
-mod magnitude {
+mod pq {
 
-    pub struct Magnitude<const N: usize, T: Copy> {
-        buf: [T; N], // Store the N largest elements, smallest to largest.
+    pub struct BoundedPriorityQueue<T, const N: usize> {
+        buf: [T; N],
 
         // `buf` at the time of writing is uninitialized with MaybeUninit.
         // We keep track of how may times insert has been called; once `N`
@@ -10,7 +10,7 @@ mod magnitude {
         num_initialized: usize,
     }
 
-    impl<const N: usize, T: Copy> Magnitude<N, T> {
+    impl<T, const N: usize> BoundedPriorityQueue<T, N> {
         fn new() -> Self {
             let buf: [T; N] = unsafe { core::mem::MaybeUninit::zeroed().assume_init() };
             Self {
@@ -19,25 +19,69 @@ mod magnitude {
             }
         }
 
-        fn index(&self, x: &T) -> Option<usize> {
-            todo!("Compute the correct index.");
-        }
-
-        pub fn insert(&mut self, x: T) {
-            if let Some(i) = self.index(&x) {
-                for j in 0..i {
-                    *self.buf.get_mut(j).unwrap() = *self.buf.get(j + 1).unwrap();
+        pub fn insert_by_key<F, R>(&mut self, x: T, f: F)
+        where
+            T: Copy,
+            R: Ord,
+            F: Fn(&T) -> R,
+        {
+            // Compute the index for where `x` should reside.
+            let index = || -> Option<usize> {
+                for (i, v) in self.buf.iter().enumerate() {
+                    if f(&x) > f(v) {
+                        continue;
+                    } else {
+                        // If this underflows, then `x` is smaller than the smallest value -- thus `None`.
+                        return i.checked_sub(1);
+                    }
                 }
 
-                *self.buf.get_mut(i).unwrap() = x;
+                // The entire for loop completed; thus `x` is the largest value.
+                return N.checked_sub(1);
+            };
+
+            // If we haven't inserted N values, we insert regardless. If we've inserted N,
+            // we need to sort the underlying arr. And if we've inserted > N values, our
+            // insertion works like a normal priority queue.
+            if self.num_initialized < N {
+                let mem = self.buf.get_mut(self.num_initialized).unwrap();
+                *mem = x;
+
+                self.num_initialized += 1;
+                if self.num_initialized == N {
+                    self.buf.sort_unstable_by_key(f);
+                }
+            } else {
+                // If this `x` value belongs in the structure, we find the index it resides at,
+                // then push all of those values to the left, dropping the smallest value.
+                if let Some(idx) = index() {
+                    for i in 0..idx {
+                        let r_mem = *self.buf.get(i + 1).unwrap();
+                        let l_mem = self.buf.get_mut(i).unwrap();
+                        *l_mem = r_mem;
+                    }
+
+                    let mem = self.buf.get_mut(idx).unwrap();
+                    *mem = x;
+                }
             }
+        }
+
+        pub fn insert(&mut self, x: T)
+        where
+            T: Ord + Copy,
+        {
+            let f = |x: &T| *x;
+            self.insert_by_key(x, f)
         }
     }
 
-    impl<const N: usize, T: Copy> IntoIterator for Magnitude<N, T> {
+    impl<T, const N: usize> IntoIterator for BoundedPriorityQueue<T, N> {
         type Item = T;
         type IntoIter = core::array::IntoIter<T, N>;
 
+        // TODO: Handle this case when we haven't triggered `num_initialized`, or the `num_initialized` sort?
+        // An option is to just panic.
         fn into_iter(self) -> Self::IntoIter {
             self.buf.into_iter()
         }
@@ -51,14 +95,18 @@ mod magnitude {
         fn test_i32() {
             const N: usize = 3;
 
-            let mut s: Magnitude<N, i32> = Magnitude::new();
+            let mut s: BoundedPriorityQueue<i32, N> = BoundedPriorityQueue::new();
             let mut a = [-5i32, 5i32, 0i32, -2i32, 2i32, 1i32, -99i32, 100i32];
             for b in a.into_iter() {
                 s.insert(b);
             }
 
+            // We place the largest values at the front with `sort` into `reverse`;
+            // We have to then reverse the iterator after taking `N`: the BPQ stores
+            // values from smallest to largest.
             a.sort();
-            for (a, b) in a.into_iter().take(N).zip(s.into_iter()) {
+            a.reverse();
+            for (a, b) in a.into_iter().take(N).rev().zip(s.into_iter()) {
                 assert_eq!(a, b);
             }
         }
