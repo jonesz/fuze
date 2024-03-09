a4cbf4f7a4df0d4b273ba8ac8d108c87200ce31a
Ethan Jones <etn.jones@gmail.com>
2024-03-09 13:38:54 -0600

    An implementation that produces (k, m) with a single iterator.
    There's a TODO here that should be benchmarked later. Does the Rust
    compiler "complete" the `map`, placing the entire result onto the
    stack? It'd likely be better to compute the parts of the sum after
    each iteration of the map.
    
    A benchmark should be produced that tests this behavior.

--- src/comb.rs
+++ src/comb.rs
@@ -4,27 +4,24 @@ pub fn comb_dempster_q<const N: usize, S>(bba_s: [&[(S, f32)]; N], q: S) -> f32
 where
     S: Copy + crate::set::SetOperations,
 {
-    let p = product::CartesianProduct::new(bba_s);
-
-    let mut k = 0.0f32;
-    let mut s = 0.0f32;
-
-    for item in p {
-        let (set, mass) = item
-            .into_iter()
-            .reduce(|(acc_s, acc_m), (s, m)| (acc_s.intersection(&s), acc_m * m))
-            .unwrap();
-
-        if set.is_empty() {
-            k += mass;
-        } else {
-            if set.is_subset(&q) && q.is_subset(&set) {
-                s += mass;
+    // TODO: Does this first `map` "complete", placing a massive value onto the
+    // stack? The below `fold` can be computed for each iteration of the `map`.
+    let (k, m) = product::CartesianProduct::new(bba_s)
+        .map(|item| {
+            item.into_iter()
+                .reduce(|(acc_s, acc_m), (set, mass)| (acc_s.intersection(&set), acc_m * mass))
+                .unwrap()
+        })
+        .fold((0.0f32, 0.0f32), |(acc_k, acc_m), (set, mass)| {
+            // K = Sum{A = \empty} m; M = Sum{A = Q} m.
+            match (set.is_empty(), set.is_subset(&q) && q.is_subset(&set)) {
+                (true, false) => (acc_k + mass, acc_m),
+                (false, true) => (acc_k, acc_m + mass),
+                (_, _) => (acc_k, acc_m),
             }
-        }
-    }
+        });
 
-    (1.0 / (1.0 - k)) * s
+    (1.0 / (1.0 - k)) * m
 }
 
 #[cfg(test)]
@@ -32,5 +29,22 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_comb_dempster_q() {}
+    fn test_comb_dempster_q() {
+        // https://en.wikipedia.org/wiki/Dempster%E2%80%93Shafer_theory#Example_producing_correct_results_in_case_of_high_conflict
+        const FILM_X: usize = 0b001;
+        const FILM_Y: usize = 0b010;
+        const FILM_Z: usize = 0b100;
+
+        const FILMS_HIGH_CONFLICT: [&[(usize, f32)]; 2] = [
+            &[(FILM_X, 0.99f32), (FILM_Y, 0.01f32)],
+            &[(FILM_Z, 0.99f32), (FILM_Y, 0.01f32)],
+        ];
+
+        // TODO: Determine what this epsilon should be.
+        let eps = 0.001f32;
+
+        assert!((comb_dempster_q(FILMS_HIGH_CONFLICT, FILM_Y) - 1.0f32).abs() < eps);
+        assert!(comb_dempster_q(FILMS_HIGH_CONFLICT, FILM_X) < eps);
+        assert!(comb_dempster_q(FILMS_HIGH_CONFLICT, FILM_Z) < eps);
+    }
 }
