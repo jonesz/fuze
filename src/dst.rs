052a8d3507cbde0d48756bd14943f64c84d990d9
Ethan Jones <etn.jones@gmail.com>
2024-03-10 01:12:23 -0600

    `bel` test.

--- src/dst.rs
+++ src/dst.rs
@@ -15,4 +15,29 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_bel() {
+        const RED: usize = 0b100;
+        const YELLOW: usize = 0b010;
+        const GREEN: usize = 0b001;
+
+        let bba = &[
+            (RED, 0.35f32),
+            (YELLOW, 0.25f32),
+            (GREEN, 0.15f32),
+            (RED | YELLOW, 0.06f32),
+            (RED | GREEN, 0.05f32),
+            (YELLOW | GREEN, 0.04f32),
+            (RED | YELLOW | GREEN, 0.1f32),
+        ];
+
+        assert_eq!(bel(bba, &RED), 0.35f32);
+        assert_eq!(bel(bba, &YELLOW), 0.25f32);
+        assert_eq!(bel(bba, &GREEN), 0.15f32);
+        assert_eq!(bel(bba, &(RED | YELLOW)), 0.66f32);
+        assert_eq!(bel(bba, &(RED | GREEN)), 0.55f32);
+        assert_eq!(bel(bba, &(YELLOW | GREEN)), 0.44f32);
+        assert_eq!(bel(bba, &(RED | YELLOW | GREEN)), 1.0f32);
+    }
 }
