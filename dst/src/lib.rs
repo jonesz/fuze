48b06e08251165ceb7549c52176cefef92690436
Ethan Jones <etn.jones@gmail.com>
2024-04-04 16:23:15 -0500

    Move DST related code into a specific `dst/` dir.

---
+++ dst/src/lib.rs
@@ -0,0 +1,7 @@
+#![cfg_attr(not(test), no_std)]
+#![warn(missing_docs)]
+mod approx;
+mod comb;
+mod dst;
+mod product;
+mod set;
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![cfg_attr(not(test), no_std)]
-#![warn(missing_docs)]
-mod approx;
-mod comb;
-mod dst;
-mod product;
-mod set;
