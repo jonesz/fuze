3fc740517ee6897a3df839f50fcfaa553cafd8da
Ethan Jones <etn.jones@gmail.com>
2024-04-05 17:44:51 -0500

    Introduce a Copy to evaluate the bottom values.

--- agg-exp/src/lib.rs
+++ agg-exp/src/lib.rs
@@ -7,22 +7,27 @@ pub trait Expert<P> {
     fn predict(&mut self, t: usize) -> P;
 }
 
-pub fn weighted_average_convex<P, W, const N: usize>(
-    predictions: &[P; N],
-    weights: &[W; N],
-    t: usize,
-) -> P
+pub fn weighted_average_convex<P, W, const N: usize>(predictions: &[P; N], weights: &[W; N]) -> P
 where
-    for <'a> &'a W: Mul<&'a P, Output = P>,
-    P: Add<Output=P> + Div<Output=P>,
+    for<'a> &'a W: Mul<&'a P, Output = P>,
+    P: Add<Output = P> + Div<Output = P> + Copy,
 {
     // $\hat{p} = \sum_{i=1}{N} w_{i,t-1} f_{i,t} / \sum_{j=1}{N} w_{j,t-1}$
     let top = weights
-        .into_iter()
+        .iter()
         .zip(predictions)
         .map(|(w_i, f_i)| w_i * f_i)
         .reduce(|a, b| a + b)
         .unwrap();
-    let bot = predictions.into_iter().reduce(|a, b| a + b).unwrap();
+
+    let bot = predictions
+        .iter()
+        // TODO: So we're iterating over `&P`; the reduction isn't fun because
+        // we can compute `&P + P`, `&P + &P`, or `P + P`, so we introduce
+        // a copy before. Can we do this without the copy?
+        .map(|a| *a)
+        .reduce(|a, b| a + b)
+        .unwrap();
+
     top / bot
 }
