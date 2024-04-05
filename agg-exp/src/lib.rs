843ec582220a9e6ea18464e22c765f8c73076221
Ethan Jones <etn.jones@gmail.com>
2024-04-05 17:10:49 -0500

    Scratch that, split into a specific convex function.

--- agg-exp/src/lib.rs
+++ agg-exp/src/lib.rs
@@ -7,28 +7,22 @@ pub trait Expert<P> {
     fn predict(&mut self, t: usize) -> P;
 }
 
-pub fn weighted_average<P, W, F, const N: usize>(
+pub fn weighted_average_convex<P, W, const N: usize>(
     predictions: &[P; N],
     weights: &[W; N],
     t: usize,
-    convex: Option<F>,
 ) -> P
 where
-    F: Fn([W; N], [P; N]) -> P,
-    W: Mul<P, Output = P>,
-    P: Add<Output = P> + Div<Output = P>,
+    &W: Mul<P, Output = P>,
+    &P: Add<Output = P> + Div<Output = P>,
 {
-    if let convex_f = Some(convex) {
-        // $\hat{p} = \sum_{i=1}{N} w_{i,t-1} f_{i,t} / \sum_{j=1}{N} w_{j,t-1}$
-        let top = weights
-            .into_iter()
-            .zip(predictions)
-            .map(|(w_i, f_i)| w_i * f_i)
-            .reduce(|a, b| a + b)
-            .unwrap();
-        let bot = predictions.into_iter().reduce(|a, b| a + b).unwrap();
-        top / bot
-    } else {
-        todo!("The function isn't convex; we need to utilize randomized prediction.");
-    }
+    // $\hat{p} = \sum_{i=1}{N} w_{i,t-1} f_{i,t} / \sum_{j=1}{N} w_{j,t-1}$
+    let top = weights
+        .into_iter()
+        .zip(predictions)
+        .map(|(w_i, f_i)| w_i * f_i)
+        .reduce(|a, b| a + b)
+        .unwrap();
+    let bot = predictions.into_iter().reduce(|a, b| a + b).unwrap();
+    top / bot
 }
