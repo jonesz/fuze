cf03e195a22118bf9a46120b8c760c5dbc9fe04e
Ethan Jones <etn.jones@gmail.com>
2024-04-06 21:32:15 -0500

    Utilize sum for l2, drop the Copy/Add requirements.

--- agg-exp/src/loss.rs
+++ agg-exp/src/loss.rs
@@ -1,5 +1,8 @@
-use core::ops::{Add, Mul, Sub};
+//! Loss functions.
+use core::ops::{Mul, Sub};
+use core::iter::Sum;
 
+/// L1 loss.
 pub fn l1<P>(t: &[P], p: &[P]) -> P {
     todo!();
 }
@@ -8,16 +11,9 @@ pub fn l1<P>(t: &[P], p: &[P]) -> P {
 pub fn l2<P>(t: &[P], p: &[P]) -> P
 where
     for<'a> &'a P: Sub<&'a P, Output = P>,
-    P: Mul<Output = P> + Add<Output = P> + Copy,
+    P: Mul<Output = P> + Sum,
 {
     t.iter()
         .zip(p)
-        .map(|(y_t, p_t)| y_t - p_t)
-        // TODO: This requires a Copy trait, could be pushed upwards
-        // into map as `(y_t - p_t) * (y_t - p_t)`. If that's done,
-        // the Copy trait can be removed.
-        .map(|a| a * a)
-        // TODO: Utilize `.sum()`.
-        .reduce(|a, b| a + b)
-        .unwrap()
+        .map(|(y_t, p_t)| (y_t - p_t) * (y_t - p_t)).sum()
 }
