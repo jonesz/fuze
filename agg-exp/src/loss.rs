2cb7ac346ebab2a2387f3a5093acf0a3ee6003b5
Ethan Jones <etn.jones@gmail.com>
2024-04-06 21:39:45 -0500

    Test for l2 loss.

--- agg-exp/src/loss.rs
+++ agg-exp/src/loss.rs
@@ -13,7 +13,21 @@ where
     for<'a> &'a P: Sub<&'a P, Output = P>,
     P: Mul<Output = P> + Sum,
 {
+    // TODO: Handle unsigned types (usize could underflow!).
     t.iter()
         .zip(p)
         .map(|(y_t, p_t)| (y_t - p_t) * (y_t - p_t)).sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_l2() {
+        let t = &[0i32, 1, 2, 3, 4];
+        let p = &[5i32, 6, 7, 8, 9];
+        let r = 125i32;
+        assert_eq!(l2(t, p), r);
+    }
 }
