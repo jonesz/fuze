25792ac3e94ae4730831ec2f0bfbf5eef7259b40
Ethan Jones <etn.jones@gmail.com>
2024-04-09 15:57:20 -0500

    EWAF prediction.
    The exponential forecaster only requires the `t-1` weight
    (according to PLG, at least). See PLG pg. 14.

--- agg-exp/src/forecaster.rs
+++ agg-exp/src/forecaster.rs
@@ -15,9 +15,10 @@ mod exp {
 
     impl<const N: usize> EWAF<f32, N> {}
 
-    impl<const N: usize> Forecaster<f32, N> for EWAF<f32, N> {
-        fn predict(&self, expert: &[f32; N]) -> f32 {
-            todo!()
+    impl<const N: usize> ExpertForecaster<f32, N> for EWAF<f32, N> {
+        fn predict(&self, experts: &[f32; N]) -> f32 {
+            // \hat{p_t} = \sum_{i=1}^{N} w_{i,t-1} f_{i,t} (PLG - pg. 14)
+            self.w.iter().zip(experts).map(|(w, f)| w * f).sum()
         }
 
         fn update(&mut self, expert: &[f32; N], revealed: &f32) {
