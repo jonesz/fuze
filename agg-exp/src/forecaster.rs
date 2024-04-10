e565bc63071ef0f60716b517acb30679fa6c6d0f
Ethan Jones <etn.jones@gmail.com>
2024-04-09 20:32:14 -0500

    Loss function as a generic parameter of EWAF.

--- agg-exp/src/forecaster.rs
+++ agg-exp/src/forecaster.rs
@@ -7,21 +7,35 @@ trait ExpertForecaster<P, const N: usize> {
 
 mod exp {
     use super::*;
+    use core::marker::PhantomData;
 
     /// The Exponentially Weighted Average Forecaster.
-    struct EWAF<W, const N: usize> {
+    struct EWAF<L, W, const N: usize>
+    where
+        L: Loss<W>,
+    {
         w: [W; N],
+        phantom: PhantomData<L>,
     }
 
-    impl<const N: usize> Default for EWAF<f32, N> {
+    impl<L, const N: usize> EWAF<L, f32, N> where L: Loss<f32> {}
+
+    impl<L, const N: usize> Default for EWAF<L, f32, N>
+    where
+        L: Loss<f32>,
+    {
         fn default() -> Self {
-            Self { w: [1.0; N] }
+            Self {
+                w: [1.0; N],
+                phantom: PhantomData,
+            }
         }
     }
 
-    impl<const N: usize> EWAF<f32, N> {}
-
-    impl<const N: usize> ExpertForecaster<f32, N> for EWAF<f32, N> {
+    impl<L, const N: usize> ExpertForecaster<f32, N> for EWAF<L, f32, N>
+    where
+        L: Loss<f32>,
+    {
         fn predict(&self, experts: &[f32; N]) -> f32 {
             // \hat{p_t} = \sum_{i=1}^{N} w_{i,t-1} f_{i,t} (PLG - pg. 14)
             self.w.iter().zip(experts).map(|(w, f)| w * f).sum()
