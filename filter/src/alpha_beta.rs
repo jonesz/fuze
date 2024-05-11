2d590f8dd5dc97a94d9c2c90755b5ed35d42e1c5
Ethan Jones <etn.jones@gmail.com>
2024-05-11 10:14:17 -0500

    Intermediate check-in.
    I'm coming back to this after four weeks and I don't know what
    it does. At some point, come back to this.

--- filter/src/alpha_beta.rs
+++ filter/src/alpha_beta.rs
@@ -1,3 +1,4 @@
+//! An alpha-beta filter from Basic Radar Tracking by Budge.
 use crate::Filter;
 
 struct AlphaBeta<P> {
@@ -8,25 +9,46 @@ struct AlphaBeta<P> {
 impl AlphaBeta<f32> {
     /// The state transition matrix: $F(n-1)$.
     fn state_transition_matrix(update_period: f32) -> [[f32; 2]; 2] {
-        [[1.0f32, update_period], [0.0f32, 1.0f32]]
+        [[1.0f32, update_period], [0.0f32, 1.0f32]] // (3.5)
     }
 
-    // The weighting matrix, $K(n)$.
-    fn weighting_matrix(update_period: f32) -> [f32; 2] {
-        [alpha, beta/update_period]
+    /// The weighting matrix, $K(n)$.
+    fn weighting_matrix(&self, update_period: f32) -> [f32; 2] {
+        [self.alpha, self.beta / update_period] // (3.10)
     }
 }
 
 impl Filter<2, f32> for AlphaBeta<f32> {
-    fn predict(update_period: f32, F: &[[f32; 2]; 2], X_s: &[f32; 2]) -> [f32; 2] {
+    fn predict(update_period: f32, X_s: &[f32; 2]) -> [f32; 2] {
         let mut X_p = [0.0f32; 2];
-        let fN = Self::state_transition_matrix(update_period);
-
-        for i in 0..2 {
-            // $X_p(n) = F(n-1) X_s(n-1)$ (3.1).
-            X_p[i] = fN[i].iter().zip(X_s).map(|(a, b)| a + b).sum();
+        // $X_p(n) = F(n-1) X_s(n-1)$ (3.1).
+        for (X_p_i, fN_i) in X_p
+            .iter_mut()
+            .zip(Self::state_transition_matrix(update_period))
+        {
+            // We're iterating over the row, compute $fN_i * X_s^T$.
+            *X_p_i = fN_i.iter().zip(X_s).map(|(a, b)| a + b).sum();
         }
 
         X_p
+    }
+
+    fn update(update_period: f32, X_p: &[f32; 2], y: &[f32; 2]) -> [f32; 2] {
+        // $X_s(n) = X_p(n) + K(n) * (y(n) - y_p(n))$ (3.2)
+
+        // $y_p(n) = H^T * X_p(n)$
+        let mut y_p = [0.0f32; 2];
+        let H_t = [1.0f32, 0.0f32];
+        for (y_p_i, (H_t_i, X_p_i)) in y_p.iter_mut().zip(H_t.iter().zip(X_p)) {
+            *y_p_i = H_t_i * X_p_i;
+        }
+
+        // $y_i - y_p_i$
+        let mut residual = [0.0f32; 2];
+        for (r_i, (y_i, y_p_i)) in residual.iter_mut().zip(y.iter().zip(y_p)) {
+            *r_i = y_i - y_p_i;
+        }
+
+        todo!()
     }
 }
--- filter/src/lib.rs
+++ filter/src/lib.rs
@@ -1,6 +1,6 @@
 mod alpha_beta;
 
 trait Filter<const N: usize, P> {
-    fn predict(T: P, F: &[[P; N]; N], X_s: &[P; N]) -> [P; N];
-    // fn update(X_p: &[T; N], y: &[T; N], y_p: &[T; N]) -> [T; N];
+    fn predict(update_period: P, X_s: &[P; N]) -> [P; N];
+    fn update(update_period: P, X_p: &[T; N], y: &[T; N]) -> [T; N];
 }
