8f9ab3e27c2572e78ae1a5c4fd396bb38544918a
Ethan Jones <etn.jones@gmail.com>
2024-05-12 19:44:01 -0500

    Move from the EECS notes implementation to the PLG equations.

--- agg-exp/src/forecaster.rs
+++ agg-exp/src/forecaster.rs
@@ -18,6 +18,7 @@ pub trait ExpertForecaster<P, const N: usize> {
     fn update(&mut self, experts: &[P; N], revealed: &P);
 }
 
+/// Code related to the Exponential Weighted Average Forecaster.
 pub mod exp {
     use super::*;
     use core::marker::PhantomData;
@@ -53,33 +54,36 @@ pub mod exp {
         }
     }
 
+    // The implementation from PLG.
     impl<L, const N: usize> ExpertForecaster<f32, N> for EWAF<L, f32, N>
     where
         L: Loss<f32, Event = f32, Cost = f32>,
     {
         fn predict(&self, experts: &[f32; N]) -> f32 {
-            // \hat{p_t} = \sum_{i=1}^{N} w_{i,t-1} f_{i,t} (PLG - pg. 14)
-            self.w.iter().zip(experts).map(|(w, f)| w * f).sum::<f32>() / self.w.iter().sum::<f32>()
+            // \hat{p_t} = \sum_{i=1}^{N} w_{i,t-1} f_{i,t} - (PLG - pg. 14)
+            self.w.iter().zip(experts).map(|(w, f)| w * f).sum::<f32>()
         }
 
-        fn update(&mut self, expert: &[f32; N], revealed: &f32) {
+        fn update(&mut self, experts: &[f32; N], revealed: &f32) {
+            // There's no `exp()` within no-std, approximate it here.
+            // TODO: It'd be nice to have access to a HAL, or something defined by linking
+            // at compile-time.
             fn approx_exp(x: f32) -> f32 {
-                // https://math.stackexchange.com/a/56064
-                // exp(z) = \frac{(z+3)^2 + 3}{(z-3)^2 + 3}
+                // https://math.stackexchange.com/a/56064 ; e^z = \frac{(z+3)^2 + 3}{(z-3)^2 + 3}
                 ((x + 3.0) * (x + 3.0) + 3.0) / ((x - 3.0) * (x - 3.0) + 3.0)
             }
 
+            // w_{i,t} = \frac{w_{i,t-1} e^{-\eta \l(f_{i,t}, y_t)}}
+            //                {\sum_{j=1}^N w_{j,t-1} e^{-\eta \l(f_{j,t-1},y_t)}}
+            // (PLG - pg.14)
             let eta = self.eta();
-            // This is pulled from notes of EECS 598 - Fall 2013, 'Prediction and Learning:
-            // It's Only a Game'; specifically 'Lecture 3, 9/11: The Exponential Weights Algorithm.'
-            // See '3-3 The Exponential Weight Algorithm' (pg. 3).
-            for (w_i, p_i) in self.w.iter_mut().zip(expert) {
-                *w_i *= approx_exp(-1.0f32 * eta * L::l(p_i, revealed));
+            let bot = self.w.iter().zip(experts).map(|(w, f)| w * f).sum::<f32>();
+            for (w_i, p_i) in self.w.iter_mut().zip(experts) {
+                *w_i *= approx_exp(-1.0f32 * eta * L::l(p_i, revealed)) / bot;
             }
         }
     }
+
     #[cfg(test)]
-    mod test {
-        use super::*;
-    }
+    mod test {}
 }
