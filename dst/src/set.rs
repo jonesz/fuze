f545a40e967e96b849b73d88321a3bd2087d85ae
Ethan Jones <etn.jones@gmail.com>
2024-06-01 16:15:08 -0500

    Beginnings of a refactor for Interval.

--- dst/src/set.rs
+++ dst/src/set.rs
@@ -33,75 +33,58 @@ impl SetOperations for usize {
     }
 }
 
-/*
 mod interval {
     use super::SetOperations;
     use core::cmp::Ord;
 
-    pub struct Interval<T: Ord, const N: usize> {
-        buf: [Option<(T, T)>; N],
+    #[derive(Debug)]
+    pub struct Interval<const N: usize, T> {
+        buf: [(T, T); N],
     }
 
-    impl<T: Ord + Copy, const N: usize> Interval<T, N> {
-        fn intersect(lhs: &(T, T), rhs: &(T, T)) -> Option<(T, T)> {
-            // Place the interval with the smallest left bound on the left.
-            let (lhs, rhs) = if lhs.0 <= rhs.0 {
-                (lhs, rhs)
-            } else {
-                (rhs, lhs)
-            };
-
-            // If there's no overlap between the intervals, lhs.1 should be < rhs.0.
-            if lhs.1 < rhs.0 {
-                return None;
-            }
-
-            // Otherwise, the intersection is the maximum of the left bound and the minimum
-            // of the right.
-            Some((lhs.0.max(rhs.0), lhs.1.min(rhs.1)))
-        }
-    }
-
-    impl<T: Ord + Copy, const N: usize> SetOperations for Interval<T, N> {
-        /// Return whether this is a subset of RHS.
-        fn is_subset(&self, rhs: &Self) -> bool {
-            self.buf
-                .iter()
-                .zip(rhs.buf.iter())
-                .all(|(left, right)| match (left, right) {
-                    (None, None) => true,
-                    // Example: [1, 4] \subset [0, 5]
-                    (Some(l), Some(r)) => l.0 >= r.0 && l.1 <= r.1,
-                    _ => false,
-                })
-        }
-
-        /// Return whether this is the empty set.
-        fn is_empty(&self) -> bool {
-            // The empty set should have all None (disjoint) values.
-            self.buf.iter().all(|x| x.is_none())
-        }
-
-        /// Return the intersection between this and rhs.
-        fn intersection(&self, rhs: &Self) -> Self {
-            let mut buf = [None; N];
-            for (idx, mem) in buf.iter_mut().enumerate() {
-                let (left, right) = (self.buf.get(idx).unwrap(), rhs.buf.get(idx).unwrap());
-                match (left, right) {
-                    (Some(l), Some(r)) => *mem = Self::intersect(l, r),
-                    _ => *mem = None,
-                }
-            }
+    impl<const N: usize, T> Interval<N, T> {
+        pub fn build(buf: [(T, T); N]) -> Self
+        where
+            T: PartialOrd,
+        {
+            assert!(buf.iter().all(|(lhs, rhs)| lhs <= rhs));
             Self { buf }
         }
 
-        /// Return the empty set.
-        fn empty() -> Self {
-            Self { buf: [None; N] }
+        fn is_subset(&self, rhs: &Self) -> bool
+        where
+            T: PartialOrd,
+        {
+            let f = |l: &(T, T), r: &(T, T)| -> bool { l.0 >= r.0 && l.1 <= r.1 };
+            self.buf.iter().zip(rhs.buf.iter()).all(|(l, r)| f(l, r))
         }
 
-        fn not(&self) -> Self {
-            todo!();
+        fn op<F>(lhs: &Self, rhs: &Self, f: F) -> Self
+        where
+            F: Fn(&(T, T), &(T, T)) -> (T, T),
+            T: Default + Copy,
+        {
+            let mut x: [(T, T); N] = [(T::default(), T::default()); N];
+            x.iter_mut()
+                .zip(lhs.buf.iter().zip(rhs.buf.iter()))
+                .for_each(|(mem, (l, r))| *mem = f(l, r));
+            return Self { buf: x };
+        }
+
+        fn cap(&self, rhs: &Self) -> Self
+        where
+            T: Ord + Default + Copy,
+        {
+            let f = |l: &(T, T), r: &(T, T)| -> (T, T) { (T::max(l.0, r.0), T::min(l.1, r.1)) };
+            Self::op(self, rhs, f)
+        }
+
+        fn cup(&self, rhs: &Self) -> Self
+        where
+            T: Ord + Default + Copy,
+        {
+            let f = |l: &(T, T), r: &(T, T)| -> (T, T) { (T::min(l.0, r.0), T::max(l.1, r.1)) };
+            Self::op(self, rhs, f)
         }
     }
 
@@ -110,30 +93,9 @@ mod interval {
         use super::*;
 
         #[test]
-        fn test_intersect_i32() {
-            // Intervals are disjoint.
-            let (a, b) = ((-1i32, 0i32), (1i32, 2i32));
-            assert!(Interval::<i32, 1>::intersect(&a, &b).is_none());
-
-            // Interval a is contained within interval b.
-            let (a, b) = ((-1i32, 1i32), (-2i32, 2i32));
-            assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == a));
-
-            // Interval b is contained within interval a.
-            let (a, b) = ((-2i32, 2i32), (-1i32, 1i32));
-            assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == b));
-
-            // Interval a overlaps with interval b on the left side.
-            let (a, b) = ((-2i32, 1i32), (0i32, 2i32));
-            assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == (0i32, 1i32)));
-
-            // Interval b overlaps with interval a on the left side.
-            let (a, b) = ((-2i32, 1i32), (-3i32, 0i32));
-            assert!(Interval::<i32, 1>::intersect(&a, &b).is_some_and(|c| c == (-2i32, 0i32)));
-        }
+        fn test_interval() {}
     }
 }
-*/
 
 #[cfg(test)]
 mod tests {
