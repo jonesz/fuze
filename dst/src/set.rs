69d4938900b22c808490187115cc0919bd747a93
Ethan Jones <etn.jones@gmail.com>
2024-06-01 18:57:53 -0500

    Work with a structure where each dimension is wrapped in an `Option`.

--- dst/src/set.rs
+++ dst/src/set.rs
@@ -39,15 +39,17 @@ mod interval {
 
     #[derive(Debug)]
     pub struct Interval<const N: usize, T> {
-        buf: [(T, T); N],
+        // Consider the case where we compute the intersection and a certain dimension
+        // is disjoint; we signify this by wrapping each dimension in an `Option`.
+        buf: [Option<(T, T)>; N],
     }
 
     impl<const N: usize, T> Interval<N, T> {
-        pub fn build(buf: [(T, T); N]) -> Self
+        pub fn build(buf: [Option<(T, T)>; N]) -> Self
         where
             T: PartialOrd,
         {
-            assert!(buf.iter().all(|(lhs, rhs)| lhs <= rhs));
+            assert!(buf.iter().flatten().all(|(lhs, rhs)| lhs <= rhs));
             Self { buf }
         }
 
@@ -55,36 +57,53 @@ mod interval {
         where
             T: PartialOrd,
         {
+            // ex: `[-1, 5] \subset [-2, 6]`
             let f = |l: &(T, T), r: &(T, T)| -> bool { l.0 >= r.0 && l.1 <= r.1 };
-            self.buf.iter().zip(rhs.buf.iter()).all(|(l, r)| f(l, r))
+            self.buf.iter().zip(rhs.buf.iter()).all(|(l, r)| {
+                (l.is_none() && r.is_none()) // Either the dimensions are both `None`...
+                    || l.as_ref()
+                        .zip(r.as_ref()) // Or they're both `Some` with the subset condition holding.
+                        .is_some_and(|(l_i, r_i)| f(l_i, r_i))
+            })
         }
 
-        fn op<F>(lhs: &Self, rhs: &Self, f: F) -> Self
+        fn binop<F>(lhs: &Self, rhs: &Self, f: F) -> Self
         where
-            F: Fn(&(T, T), &(T, T)) -> (T, T),
-            T: Default + Copy,
+            F: Fn(Option<&(T, T)>, Option<&(T, T)>) -> Option<(T, T)>,
         {
-            let mut x: [(T, T); N] = [(T::default(), T::default()); N];
+            let mut x: [Option<(T, T)>; N] = core::array::from_fn(|_| None);
             x.iter_mut()
                 .zip(lhs.buf.iter().zip(rhs.buf.iter()))
-                .for_each(|(mem, (l, r))| *mem = f(l, r));
-            return Self { buf: x };
+                .for_each(|(mem, (l, r))| *mem = f(l.as_ref(), r.as_ref()));
+            Self { buf: x }
         }
 
         fn cap(&self, rhs: &Self) -> Self
         where
-            T: Ord + Default + Copy,
+            T: Ord + Copy,
         {
-            let f = |l: &(T, T), r: &(T, T)| -> (T, T) { (T::max(l.0, r.0), T::min(l.1, r.1)) };
-            Self::op(self, rhs, f)
+            let f = |l: Option<&(T, T)>, r: Option<&(T, T)>| -> Option<(T, T)> {
+                l.zip(r) // `zip` is effectively an AND, then we have the following condition.
+                    .map(|(l, r)| (T::max(l.0, r.0), T::min(l.1, r.1)))
+            };
+
+            Self::binop(self, rhs, f)
         }
 
         fn cup(&self, rhs: &Self) -> Self
         where
-            T: Ord + Default + Copy,
+            T: Ord + Copy,
         {
-            let f = |l: &(T, T), r: &(T, T)| -> (T, T) { (T::min(l.0, r.0), T::max(l.1, r.1)) };
-            Self::op(self, rhs, f)
+            let f = |l: Option<&(T, T)>, r: Option<&(T, T)>| -> Option<(T, T)> {
+                match (l, r) {
+                    (None, None) => None,
+                    (Some(_), None) => l.copied(),
+                    (None, Some(_)) => r.copied(),
+                    (Some(m), Some(s)) => Some((T::min(m.0, s.0), T::max(m.1, s.1))),
+                }
+            };
+
+            Self::binop(self, rhs, f)
         }
     }
 
