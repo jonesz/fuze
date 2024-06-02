3020cdc998a89def1b91aceac7bcdf686c434392
Ethan Jones <etn.jones@gmail.com>
2024-06-02 17:15:00 -0500

    Removal of product; implementing `Set` for `Interval`.
    A problem is arising with the `not` function. The NOT of an `Interval`
    is best represented as TWO `Intervals`. One solution is to utilize
    a flag and then match that flag, executing different code. Another
    solution might be to just allow the two intervals within an `Interval`.

--- dst/src/set.rs
+++ dst/src/set.rs
@@ -1,40 +1,17 @@
-pub trait SetOperations: Eq {
-    /// Return whether this is a subset of RHS.
+/// A mathematical Set, but specifically those operations needed for a DST impl.
+pub trait Set: PartialEq {
+    /// Compute whether the LHS is a subset of the RHS.
     fn is_subset(&self, rhs: &Self) -> bool;
-    /// Return whether this is the empty set.
-    fn is_empty(&self) -> bool;
-    /// Return the intersection between this and rhs.
-    fn intersection(&self, rhs: &Self) -> Self;
-    /// Return the empty set.
-    fn empty() -> Self;
+    /// Compute the intersection between the LHS and RHS.
+    fn cap(lhs: &Self, rhs: &Self) -> Self;
+    /// Compute the union betweenthe LHS ad RHS.
+    fn cup(lhs: &Self, rhs: &Self) -> Self;
+    /// Compute the NOT of some Set.
     fn not(&self) -> Self;
 }
 
-impl SetOperations for usize {
-    fn is_subset(&self, rhs: &Self) -> bool {
-        *self & rhs == *self
-    }
-
-    fn is_empty(&self) -> bool {
-        *self == 0usize
-    }
-
-    fn intersection(&self, rhs: &Self) -> Self {
-        self & rhs
-    }
-
-    /// Return the empty set.
-    fn empty() -> Self {
-        0usize
-    }
-
-    fn not(&self) -> Self {
-        !self
-    }
-}
-
 mod interval {
-    use super::SetOperations;
+    use super::Set;
     use core::cmp::Ord;
 
     #[derive(Debug)]
@@ -53,20 +30,6 @@ mod interval {
             Self { buf }
         }
 
-        fn is_subset(lhs: &Self, rhs: &Self) -> bool
-        where
-            T: PartialOrd,
-        {
-            // ex: `[-1, 5] \subset [-2, 6]`
-            let f = |l: &(T, T), r: &(T, T)| -> bool { l.0 >= r.0 && l.1 <= r.1 };
-            lhs.buf.iter().zip(rhs.buf.iter()).all(|(l, r)| {
-                (l.is_none() && r.is_none()) // Either the dimensions are both `None`...
-                    || l.as_ref()
-                        .zip(r.as_ref()) // Or they're both `Some` with the subset condition holding.
-                        .is_some_and(|(l_i, r_i)| f(l_i, r_i))
-            })
-        }
-
         fn binop<F>(lhs: &Self, rhs: &Self, f: F) -> Self
         where
             F: Fn(Option<&(T, T)>, Option<&(T, T)>) -> Option<(T, T)>,
@@ -77,11 +40,37 @@ mod interval {
                 .for_each(|(mem, (l, r))| *mem = f(l.as_ref(), r.as_ref()));
             Self { buf: x }
         }
+    }
 
-        fn cap(lhs: &Self, rhs: &Self) -> Self
-        where
-            T: Ord + Copy,
-        {
+    impl<const N: usize, T> PartialEq for Interval<N, T>
+    where
+        T: PartialEq,
+    {
+        fn eq(&self, rhs: &Self) -> bool {
+            self.buf.iter().zip(rhs.buf.iter()).all(|(l, r)| {
+                l.as_ref()
+                    .zip(r.as_ref())
+                    .is_some_and(|(l_i, r_i)| l_i.0 == r_i.0 && l_i.1 == r_i.1)
+            })
+        }
+    }
+
+    impl<const N: usize, T> Set for Interval<N, T>
+    where
+        T: PartialOrd + Ord + Copy,
+    {
+        fn is_subset(&self, rhs: &Self) -> bool {
+            // ex: `[-1, 5] \subset [-2, 6]`
+            let f = |l: &(T, T), r: &(T, T)| -> bool { l.0 >= r.0 && l.1 <= r.1 };
+            self.buf.iter().zip(rhs.buf.iter()).all(|(l, r)| {
+                (l.is_none() && r.is_none()) // Either the dimensions are both `None`...
+                    || l.as_ref()
+                        .zip(r.as_ref()) // Or they're both `Some` with the subset condition holding.
+                        .is_some_and(|(l_i, r_i)| f(l_i, r_i))
+            })
+        }
+
+        fn cap(lhs: &Self, rhs: &Self) -> Self {
             let f = |l: Option<&(T, T)>, r: Option<&(T, T)>| -> Option<(T, T)> {
                 l.zip(r) // `zip` is effectively an AND...
                     .and_then(|(l, r)| {
@@ -111,6 +100,13 @@ mod interval {
             };
 
             Self::binop(lhs, rhs, f)
+        }
+
+        fn not(&self) -> Self {
+            // TODO: The NOT of an `Interval` is two `Interval`s; a potential implementation
+            // is to utilize some flag to indicate that `subset`, `cup`, etc. should NOT their
+            // results?
+            unimplemented!();
         }
     }
 
