ccf3b3818091a6ad9e873951bb112bdfd99c18a2
Ethan Jones <etn.jones@gmail.com>
2024-07-21 18:09:38 -0500

    Dump both the old approximation and combination code.

--- dst/src/approx.rs
+++ dst/src/approx.rs
@@ -5,228 +5,63 @@
 //! difficult to do this in parallel because of potential arr irregularity.
 //! With an approximation, we can take some BBA `[?](set, f32)` and reduce it
 //! down to a known-length `[k](set, f32)`, allowing for reduce and so forth.
+
+use crate::container::heap::PriorityHeap;
 use crate::set::Set;
-use core::iter::Sum;
-use core::ops::DivAssign;
 
-/// An Approximation scheme that reduces the number of elements within a BBA.
-pub trait Approximation<S, T> {
-    /// Compute an approximation for the passed BBA.
-    fn approx<'a, const N: usize, I: IntoIterator<Item = &'a (S, T)> + Clone>(x: I) -> [(S, T); N]
-    where
-        S: 'a + Clone,
-        T: 'a + Clone;
+pub trait Approximation<S: Set, T> {
+    fn approx<const N: usize>(bba: impl IntoIterator<Item = (S, T)>) -> [(S, T); N];
 }
 
-/// BPQ structures that are useful for keeping track of the `k` largest elements.
-mod bpq {
+pub struct KX();
 
-    /// A PQ that is backed by a known-length arr.
-    pub struct BoundedPriorityQueue<T, const N: usize> {
-        buf: [Option<T>; N],
-        initialized: usize,
-    }
+impl<S: Set> Approximation<S, f32> for KX {
+    fn approx<const N: usize>(bba: impl IntoIterator<Item = (S, f32)>) -> [(S, f32); N] {
+        // Utilize a PH to capture the N largest elements within the BBA.
+        let mut container = PriorityHeap::<N, (S, f32)>::default();
+        bba.into_iter().for_each(|x| {
+            let f = |x: &(S, f32)| x.1;
+            container.insert_by_key(f, x);
+        });
 
-    impl<T, const N: usize> Default for BoundedPriorityQueue<T, N> {
-        fn default() -> Self {
-            Self {
-                buf: core::array::from_fn(|_| None),
-                initialized: 0usize,
-            }
-        }
-    }
+        // Push those N elements into the resulting approximation.
+        let mut container_iter = container.consume();
+        let mut buf = core::array::from_fn(|_| container_iter.next().unwrap_or((S::EMPTY, 0.0f32)));
 
-    impl<T, const N: usize> BoundedPriorityQueue<T, N> {
-        /// Insert a `T` into the queue by a key extraction function.
-        pub fn insert_by_key<F, R>(&mut self, x: T, f: F)
-        where
-            F: Fn(&T) -> R,
-            R: PartialOrd,
-        {
-            if self.initialized < N {
-                // TODO: Technically, we know where the `None` is: it's at `self.initialized`.
-                // If the structure hasn't been fully initialized, find the first `None` slot and insert `x`.
-                let mem = self.buf.iter_mut().find(|opt| opt.is_none()).expect(
-                    "Should have found a slot as the structure says it's not fully initialized.",
-                );
-                *mem = Some(x);
-                self.initialized += 1;
-            } else {
-                const ERROR_NONE: &str = "Buffer is supposed to be fully initialized/`Some(z)`.";
-                // Find the smallest value and replace it if the value to be inserted is larger.
-                // utilize `reduce` to avoid needing more than a `PartialOrd` bound (f32).
-                let idx = self
-                    .buf
-                    .iter()
-                    .map(|opt| f(opt.as_ref().expect(ERROR_NONE))) // Compute `R: PartialOrd` for each `&T`.
-                    .enumerate() // `(idx: usize, cmp: R)`
-                    .reduce(|acc, e| if acc.1 <= e.1 { acc } else { e })
-                    .expect(ERROR_NONE)
-                    .0;
+        // Rescale so that the resulting BBA sums to `1.0f32`.
+        let denom: f32 = buf.iter().map(|e| e.1).sum();
+        buf.iter_mut().for_each(|mem| mem.1 /= denom);
 
-                // TODO: The above iteration and the below feel terrible.
-                let mem = self
-                    .buf
-                    .get_mut(idx)
-                    .expect("The returned idx should have been within `N`")
-                    .as_mut()
-                    .expect(
-                        "The buffer should have been initialized -- this should have been Some(v)",
-                    );
-
-                if f(mem) < f(&x) {
-                    *mem = x;
-                }
-            }
-        }
-
-        /// Return the underlying buffer.
-        pub fn consume(self) -> [Option<T>; N] {
-            self.buf
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        const N: usize = 8;
-
-        // BPQ requires a key fn; for usize, just dereference.
-        fn key_fn_usize(x: &usize) -> usize {
-            *x
-        }
-
-        fn default_bpq() -> BoundedPriorityQueue<usize, N> {
-            let mut bpq = BoundedPriorityQueue::default();
-            for i in 0..8 {
-                bpq.insert_by_key(i, key_fn_usize);
-            }
-
-            bpq
-        }
-
-        #[test]
-        fn test_insert() {
-            const TEST_SET: &[usize] = &[10usize, 3, 34, 500, 66];
-            let mut bpq = default_bpq();
-
-            for i in TEST_SET.into_iter().copied() {
-                bpq.insert_by_key(i, key_fn_usize);
-            }
-
-            // Of those inserted: 10, 34, 500, 66 should exist within it.
-            let buf: [Option<usize>; N] = bpq.consume();
-
-            assert!(buf.contains(&Some(10usize)));
-            assert!(buf.contains(&Some(34usize)));
-            assert!(buf.contains(&Some(66usize)));
-            assert!(buf.contains(&Some(500usize)));
-
-            // 3 shouldn't exist in the buf, but 4-7 from the default BPQ should.
-            assert!(!buf.contains(&Some(3usize)));
-            for i in 4..7 {
-                assert!(buf.contains(&Some(i)));
-            }
-        }
+        buf
     }
 }
 
-pub struct KX;
+pub struct Summarize();
 
-impl<S, T> Approximation<S, T> for KX
-where
-    S: Set + Copy,
-    T: PartialOrd + Copy + Sum + From<u8> + DivAssign,
-{
-    // TODO: Refactor this.
-    fn approx<'a, const N: usize, I: IntoIterator<Item = &'a (S, T)> + Clone>(x: I) -> [(S, T); N]
-    where
-        S: 'a,
-        T: 'a,
-    {
-        // TODO: This is ripped from `comb`, should likely be refactored.
-        fn build_arr<S: Set, T: From<u8>, const Z: usize>() -> [(S, T); Z] {
-            core::array::from_fn(|_| (S::EMPTY, 0u8.into()))
-        }
+impl<S: Set> Approximation<S, f32> for Summarize {
+    fn approx<const N: usize>(bba: impl IntoIterator<Item = (S, f32)>) -> [(S, f32); N] {
+        // Utilize a PH to capture the N largest elements within the BBA; those that are
+        // evicted are merged together. NOTE: We technically want to store (N-1) elements,
+        // but const generics make this difficult -- we'll handle this later.
+        let mut container = PriorityHeap::<N, (S, f32)>::default();
+        let mut merged = (S::EMPTY, 0.0f32);
 
-        let mut bpq = bpq::BoundedPriorityQueue::<&'a (S, T), N>::default();
-        for elem in x.into_iter() {
-            bpq.insert_by_key(elem, |z: &&(S, T)| -> T { z.1 });
-        }
-
-        let buf = bpq.consume();
-        let bot: T = buf.iter().flatten().map(|e| e.1).sum();
-
-        let mut out = build_arr::<S, T, N>();
-        for (mem, elem) in out.iter_mut().zip(buf.iter().flatten()) {
-            *mem = **elem;
-            mem.1 /= bot;
-        }
-
-        out
-    }
-}
-
-mod approx_rw {
-    use crate::container::heap::PriorityHeap;
-    use crate::set::Set;
-
-    trait Approximation<S: Set, T> {
-        fn approx<const N: usize>(bba: impl IntoIterator<Item = (S, T)>) -> [(S, T); N];
-    }
-
-    struct KX();
-
-    impl<S: Set> Approximation<S, f32> for KX {
-        fn approx<const N: usize>(bba: impl IntoIterator<Item = (S, f32)>) -> [(S, f32); N] {
-            // Utilize a PH to capture the N largest elements within the BBA.
-            let mut container = PriorityHeap::<N, (S, f32)>::default();
-            bba.into_iter().for_each(|x| {
-                let f = |x: &(S, f32)| x.1;
-                container.insert_by_key(f, x);
-            });
-
-            // Push those N elements into the resulting approximation.
-            let mut container_iter = container.consume();
-            let mut buf =
-                core::array::from_fn(|_| container_iter.next().unwrap_or((S::EMPTY, 0.0f32)));
-
-            // Rescale so that the resulting BBA sums to `1.0f32`.
-            let denom: f32 = buf.iter().map(|e| e.1).sum();
-            buf.iter_mut().for_each(|mem| mem.1 /= denom);
-
-            buf
-        }
-    }
-
-    struct Summarize();
-
-    impl<S: Set> Approximation<S, f32> for Summarize {
-        fn approx<const N: usize>(bba: impl IntoIterator<Item = (S, f32)>) -> [(S, f32); N] {
-            // Utilize a PH to capture the N largest elements within the BBA; those that are
-            // evicted are merged together. NOTE: We technically want to store (N-1) elements,
-            // but const generics make this difficult -- we'll handle this later.
-            let mut container = PriorityHeap::<N, (S, f32)>::default();
-            let mut merged = (S::EMPTY, 0.0f32);
-
-            for elem in bba {
-                let f = |x: &(S, f32)| x.1;
-                if let Some(evicted) = container.insert_by_key(f, elem) {
-                    merged = (S::cup(&merged.0, &evicted.0), merged.1 + evicted.1);
-                }
+        for elem in bba {
+            let f = |x: &(S, f32)| x.1;
+            if let Some(evicted) = container.insert_by_key(f, elem) {
+                merged = (S::cup(&merged.0, &evicted.0), merged.1 + evicted.1);
             }
-
-            // Push those N elements into the resulting approximation.
-            let mut container_iter = container.consume();
-            let mut buf =
-                core::array::from_fn(|_| container_iter.next().unwrap_or((S::EMPTY, 0.0f32)));
-
-            // Back to N vs (N-1) -- merge the `merged` into the last element of the arr.
-            let last = buf.get(N - 1).unwrap();
-            merged = (S::cup(&merged.0, &last.0), merged.1 + last.1);
-            *buf.get_mut(N - 1).unwrap() = merged;
-
-            buf
         }
+
+        // Push those N elements into the resulting approximation.
+        let mut container_iter = container.consume();
+        let mut buf = core::array::from_fn(|_| container_iter.next().unwrap_or((S::EMPTY, 0.0f32)));
+
+        // Back to N vs (N-1) -- merge the `merged` into the last element of the arr.
+        let last = buf.get(N - 1).unwrap();
+        merged = (S::cup(&merged.0, &last.0), merged.1 + last.1);
+        *buf.get_mut(N - 1).unwrap() = merged;
+
+        buf
     }
 }
--- dst/src/comb.rs
+++ dst/src/comb.rs
@@ -1,204 +1,39 @@
 //! Combination rules for Dempster-Shafer Theory.
-use crate::approx::Approximation;
+use crate::container::em::SummationEM;
 use crate::set::Set;
-use core::marker::PhantomData;
 
-mod store {
-    use core::ops::{AddAssign, MulAssign};
-
-    /// A linear store, where insertions/gets are started from the zero'th
-    /// position.
-    pub(super) struct LinearStore<const N: usize, K, V> {
-        buf: [Option<(K, V)>; N],
-    }
-
-    impl<const N: usize, K, V> Default for LinearStore<N, K, V>
-    where
-        V: Copy,
-    {
-        fn default() -> Self {
-            Self {
-                buf: core::array::from_fn(|_| None),
-            }
-        }
-    }
-
-    impl<const N: usize, K, V> LinearStore<N, K, V>
-    where
-        K: PartialEq,
-        V: MulAssign + AddAssign + Copy,
-    {
-        /// Insert a key-value pair into the store.
-        pub fn insert(&mut self, k: K, v: V) {
-            let mem = self
-                .buf
-                .iter_mut()
-                // The iterator is sequential (0, 1, ...), so `None` will occur after all
-                // `Some(x)` have been encountered.
-                .find(|opt| opt.is_none() || opt.as_ref().is_some_and(|(a, _)| &k == a))
-                .expect("Unable to find a position; `N` const likely wrong.");
-
-            // Within `Option`, there's no clean API to `insert` here without the branch.
-            // TODO: Attempt to find one.
-            if let Some((_, m)) = mem {
-                *m += v;
-            } else {
-                *mem = Some((k, v));
-            }
-        }
-
-        /// Get the associated value for a passed key.
-        pub fn get(&self, k: &K) -> Option<&V> {
-            self.buf.iter().find_map(|opt| {
-                opt.as_ref() // `find_map` returns the first `Some(b)` encountered.
-                    .and_then(|(a, b)| if k == a { Some(b) } else { None })
-            })
-        }
-
-        /// Get the underlying buffer for this struct.
-        pub fn buf(&self) -> &[Option<(K, V)>; N] {
-            &self.buf
-        }
-
-        /// Scale the entire vector by some value.
-        pub fn scale(&mut self, v: V) {
-            self.buf.iter_mut().flatten().for_each(|(_, m)| {
-                *m *= v;
-            });
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        const N: usize = 8;
-
-        // Construct the default store for testing.
-        fn default_store() -> LinearStore<N, usize, usize> {
-            let mut store = LinearStore::<N, usize, usize>::default();
-            store.insert(0, 1);
-            store.insert(1, 2);
-            store.insert(2, 4);
-            store
-        }
-
-        #[test]
-        fn test_insert_get() {
-            let mut store = default_store();
-
-            assert!(store.get(&0).is_some());
-            assert_eq!(store.get(&0).unwrap(), &1);
-
-            assert!(store.get(&3).is_none());
-            assert_eq!(store.get(&3), None);
-
-            store.insert(0, 10);
-            assert!(store.get(&0).is_some());
-            assert_eq!(store.get(&0).unwrap(), &11);
-
-            store.insert(4, 5);
-            assert!(store.get(&4).is_some());
-            assert_eq!(store.get(&4).unwrap(), &5);
-        }
-    }
+trait CombRule<S: Set, T> {
+    fn comb<const N: usize, const N_SQR: usize>(
+        a: &[(S, T); N],
+        b: &[(S, T); N],
+    ) -> impl Iterator<Item = (S, T)>;
 }
 
-pub trait CombRule<S: Set, T> {
-    // TODO: We need 'const generic exprs' in stable to avoid the `N2` constraint...
-    /// Combine a set of BBAs where we initially compute an approximation and then reduce the
-    /// BBAs via combination w/ rule, then approximation.
-    fn comb<'a, const N: usize, const N2: usize, A>(
-        bba: impl IntoIterator<Item = impl IntoIterator<Item = &'a (S, T)> + Clone>,
-    ) -> [(S, T); N]
-    where
-        A: Approximation<S, T>,
-        S: 'a,
-        T: 'a;
-}
+struct Dempster();
 
-pub struct Dempster<S, T>(PhantomData<S>, PhantomData<T>);
-
-impl<S> CombRule<S, f32> for Dempster<S, f32>
+impl<S> CombRule<S, f32> for Dempster
 where
-    S: Set + Copy, // TODO: Get rid of the `Copy`.
+    S: Set,
 {
-    fn comb<'a, const N: usize, const N2: usize, A>(
-        bba: impl IntoIterator<Item = impl IntoIterator<Item = &'a (S, f32)> + Clone>,
-    ) -> [(S, f32); N]
-    where
-        A: Approximation<S, f32>,
-        S: 'a,
-    {
-        // TODO: See the comment within the trait about 'const generic exprs'. There's `N * N`
-        // intersections to compute between each subset after we compute the initial approximation
-        // (&[(S, f32)] -> [(S, f32); N]); these have to be placed on the stack...
-        assert!(N2 == N * N);
+    fn comb<const N: usize, const N_SQR: usize>(
+        a: &[(S, f32); N],
+        b: &[(S, f32); N],
+    ) -> impl Iterator<Item = (S, f32)> {
+        let mut conflict = 0.0f32; // K.
+        let mut map: SummationEM<N, S, f32> = SummationEM::default();
 
-        bba.into_iter()
-            .map(|e| A::approx(e)) // Compute the initial approximations.
-            .reduce(|acc, e: [(S, f32); N]| {
-                let mut store = store::LinearStore::<N2, S, f32>::default();
-                let mut k = 0.0f32; // Running conflict when B \cap C is empty.
+        for (j, k) in a.iter().flat_map(|j| b.iter().map(move |k| (j, k))) {
+            let j_cap_k = S::cap(&j.0, &k.0);
+            let j_mul_k = j.1 * k.1;
 
-                for (acc_i, e_i) in acc.iter().flat_map(|x1| e.iter().map(move |x2| (x1, x2))) {
-                    // B \cap C = m1(B) * m2(C) where B \cap C != \empty.
-                    let a_cap_e = S::cap(&acc_i.0, &e_i.0);
-                    if a_cap_e == S::EMPTY {
-                        k += acc_i.1 * e_i.1;
-                    } else {
-                        store.insert(a_cap_e, acc_i.1 * e_i.1);
-                    }
-                }
-
-                // Compute the conflict \frac{1}{1-K} and then scale the arr.
-                let conflict = 1f32 / (1f32 - k);
-                store.scale(conflict);
-
-                // Compute the approximation.
-                A::approx(store.buf().iter().flatten())
-            })
-            .expect("Zero-length iterator passed in?")
-    }
-}
-
-mod comb_rw {
-    use crate::container::em::SummationEM;
-    use crate::set::Set;
-
-    trait CombRule<S: Set, T> {
-        fn comb<const N: usize, const N_SQR: usize>(
-            a: &[(S, T); N],
-            b: &[(S, T); N],
-        ) -> impl Iterator<Item = (S, T)>;
-    }
-
-    struct Dempster();
-
-    impl<S> CombRule<S, f32> for Dempster
-    where
-        S: Set,
-    {
-        fn comb<const N: usize, const N_SQR: usize>(
-            a: &[(S, f32); N],
-            b: &[(S, f32); N],
-        ) -> impl Iterator<Item = (S, f32)> {
-            let mut conflict = 0.0f32; // K.
-            let mut map: SummationEM<N, S, f32> = SummationEM::default();
-
-            for (j, k) in a.iter().flat_map(|j| b.iter().map(move |k| (j, k))) {
-                let j_cap_k = S::cap(&j.0, &k.0);
-                let j_mul_k = j.1 * k.1;
-
-                if j_cap_k == S::EMPTY {
-                    conflict += j_mul_k;
-                } else {
-                    map.insert(j_cap_k, j_mul_k);
-                }
+            if j_cap_k == S::EMPTY {
+                conflict += j_mul_k;
+            } else {
+                map.insert(j_cap_k, j_mul_k);
             }
-
-            map.scale(1f32 / (1f32 - conflict));
-            map.consume()
         }
+
+        map.scale(1f32 / (1f32 - conflict));
+        map.consume()
     }
 }
